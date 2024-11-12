7db3d278dc4ec0ba289aaa1fa8486a4775b27f1b
inklinng <assholl72@gmail.com>
2024-11-12 21:55:00 +0200

    a

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const path = require('path')
-const port = 6942
-const app = express()
-
-app.listen(port,()=>{
-    console.log("server started")
-})
